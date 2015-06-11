2804810e154a6bce0263a56ea74362e48689dd3e
Axel Prieto <prietoaxel@gmail.com>
2015-06-10 23:32:04 -0300

    minor changes

---
+++ HackerRank/Algorithms/Warmup/12-manasa-and-stones.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+int main() {
+    int T, n, a, b;
+    
+    cin >> T;
+    
+    while(T--) {
+        cin >> n >> a >> b;
+        
+        set<int> lastStonesSet;
+        set<int>::iterator it;
+        
+        for(int i = 0; i < n; i++) {
+            lastStonesSet.insert((a * i) + (b * (n - 1 - i)));
+        }
+        
+        for (it=lastStonesSet.begin(); it!=lastStonesSet.end(); ++it) {
+            cout << *it << ' ';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
