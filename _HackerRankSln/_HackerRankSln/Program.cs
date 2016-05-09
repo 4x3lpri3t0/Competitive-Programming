73299e962658d3207045b94eeea3e2e784ff6d52
Axel Prieto <prietoaxel@gmail.com>
2016-05-08 22:54:24 -0300

    1 easy problem

--- _HackerRankSln/_HackerRankSln/Program.cs
+++ _HackerRankSln/_HackerRankSln/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _HackerRankSln
+﻿namespace _HackerRankSln
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            // Console.WriteLine(Simple_Array_Sum.ArraySum());
         }
     }
 }
