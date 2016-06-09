4965cf2fccbded592ca95845d3ce379edbee209d
Axel Prieto <prietoaxel@gmail.com>
2016-06-09 00:10:06 -0300

    Daily programming practice :)

--- CodeForces/CodeForces/Program.cs
+++ CodeForces/CodeForces/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace CodeForces
+﻿using CodeForces.Level_1___Math;
+
+namespace CodeForces
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            #region Level 1 - Math
+            //Theatre_Square.Main();
+            //Watermelon.Main();
+            #endregion
         }
     }
 }
