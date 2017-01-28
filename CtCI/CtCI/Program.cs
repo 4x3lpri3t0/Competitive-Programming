ad853e45577644405a3fce127ae0aa3c3c72ce83
Axel Prieto <prietoaxel@gmail.com>
2017-01-28 20:47:17 -0300

    Daily programming practice

---
+++ CtCI/CtCI/Program.cs
@@ -0,0 +1,20 @@
+﻿using CtCI._1___Arrays_and_Strings;
+
+namespace CtCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays and Strings
+            //_1_1_UniqueCharacters.Main();
+            //_1_2_ReverseString.Main();
+            //_1_3_IsPermutation.Main();
+            //_1_4_ReplaceSpaces.Main();
+            //_1_5_StringCompression.Main();
+            //_1_6_RotateMatrix.Main(); // REVIEW
+            _1_7_ZeroMatrix.Main();
+            #endregion
+        }
+    }
+}
