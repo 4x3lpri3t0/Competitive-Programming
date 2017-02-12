491d7a3d06b4b5c91737b84c1d6a8fdb48ea6a10
zedronar <prietoaxel@gmail.com>
2017-02-11 21:25:43 -0300

    minor changes

--- Pramp/Pramp/Problems/Array_of_Array_Products.cs
+++ Pramp/Pramp/Problems/Array_of_Array_Products.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
 
@@ -21,5 +18,58 @@ Example: given the array [2,  7,  3,  4] your function would return: [84, 24, 56
 
 public class Array_of_Array_Products
 {
+    public static void Main()
+    {
+        int[] test = { 2, 7, 3, 4 };
 
+        var result = GetProductsExceptAtIndex(test);
+        int[] expected = { 84, 24, 56, 42 };
+        Debug.Assert(Enumerable.SequenceEqual(result, expected));
+    }
+
+    private static int[] GetProductsExceptAtIndex(int[] intArray)
+    {
+        int[] productsExceptAtIndex = new int[intArray.Length];
+
+        // Find product of all the integers before it
+        int productSoFar = 1;
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            productsExceptAtIndex[i] = productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        // Find product of all the integers after it
+        productSoFar = 1;
+        for (int i = intArray.Length - 1; i >= 0; i--)
+        {
+            productsExceptAtIndex[i] *= productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        return productsExceptAtIndex;
+    }
 }
+
+/*
+
+A greedy algorithm iterates through the problem space taking the optimal solution
+"so far," until it reaches the end.
+
+The greedy approach is only optimal if the problem has "optimal substructure,"
+which means stitching together optimal solutions to subproblems yields
+an optimal solution.
+
+Complexity
+==========
+
+O(n) time and O(n) space. We make two passes through our input a list,
+and the list we build always has the same length as the input list.
+
+Learned
+=======
+
+Start with a brute force solution, look for repeat work in that solution,
+and modify it to only do that work once.
+
+*/
--- Pramp/Pramp/Problems/BST_Successor.cs
+++ Pramp/Pramp/Problems/BST_Successor.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
 
@@ -15,5 +11,99 @@ explain and code the most efficient way to find the successor of n.
 
 public class BST_Successor
 {
+    private class Node
+    {
+        public Node left;
+        public Node right;
+        public Node parent;
+        public int val;
 
+        public Node(int val, Node parent)
+        {
+            this.val = val;
+            this.parent = parent;
+        }
+    }
+
+    public static void Main()
+    {
+        Node n1 = new Node(4, null);
+
+        n1.left = new Node(2, n1);
+        n1.right = new Node(6, n1);
+
+        n1.left.left = new Node(1, n1.left);
+        n1.left.right = new Node(3, n1.left);
+
+        n1.right.left = new Node(5, n1.right);
+        n1.right.right = new Node(7, n1.right);
+
+        Node succOf3 = GetSuccessor(n1.left.right);
+        Node succOf4 = GetSuccessor(n1);
+
+        Console.WriteLine("Node 3's succesor is " + succOf3.val);
+        Console.WriteLine("Node 4's successor is " + succOf4.val);
+    }
+
+    /* 
+    *          4
+    *     2          6
+    *   1   3      5    7
+    *   
+    *   Succ 3 -> 4 
+    *   Succ 4 -> 6
+    */
+    private static Node GetSuccessor(Node node)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        if (node.right == null) // Go up
+        {
+            Node child;
+            while (node.parent != null)
+            {
+                child = node;
+                node = node.parent;
+
+                if (node.val < child.val)
+                {
+                    continue;
+                }
+                else
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+        else 
+        {
+            // There is a right node
+            // -> leftmost of it is going to be succesor
+            return GetMin(node.right);
+        }
+    }
+
+    private static Node GetMin(Node node)
+    {
+        while (node.left != null)
+        {
+            node = node.left;
+        }
+
+        return node;
+    }
 }
+
+/* Get succesor of 3:
+ * 
+ *          4
+ *     2          6
+ *   1   3  
+ *   
+ *  3 -> 2 -> 4
+ */
