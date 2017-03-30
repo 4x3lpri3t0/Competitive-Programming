78649366169fd6b0edaa7ebc66aba4122d4824ab
Axel Prieto <prietoaxel@gmail.com>
2017-03-29 22:09:03 -0300

    Daily programming practice

---
+++ InterviewCake/InterviewCake/Program.cs
@@ -0,0 +1,15 @@
+﻿using InterviewCake.Questions;
+using System.Linq;
+
+namespace InterviewCake
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //_01___stock_price.Main();
+            //_02___product_of_other_numbers.Main();
+            _03___highest_product_of_3.Main(); // TODO: Review
+        }
+    }
+}
---
+++ InterviewCake/InterviewCake/Questions/02 - product of other numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace InterviewCake.Questions
+{
+    public class _02___product_of_other_numbers
+    {
+        public static void Main()
+        {
+            int[] ints = { 1, 7, 3, 4 };
+            var result = GetProductsOfAllIntsExceptAtIndex(ints);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int[] GetProductsOfAllIntsExceptAtIndex(int[] nums)
+        {
+            int[] otherProds = new int[nums.Length];
+
+            // Find products before (left) current
+            int prodSoFar = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                otherProds[i] = prodSoFar;
+                prodSoFar *= nums[i];
+            }
+
+            // Find products after (right) current
+            prodSoFar = 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                otherProds[i] *= prodSoFar;
+                prodSoFar *= nums[i];
+            }
+
+            return otherProds;
+        }
+    }
+}
