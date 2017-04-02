cf20f7ace6462b1174e620c45da281138aa689d7
zedronar <prietoaxel@gmail.com>
2017-04-02 17:00:41 -0300

    Started Sedgewick book

---
+++ _Sedgewick/Sedgewick/src/Main.java
@@ -0,0 +1,9 @@
+import static java.lang.System.*;
+import static U1_1_BasicProgModel.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        out.println("Hello");
+    }
+}
---
+++ _Sedgewick/Sedgewick/src/U1_1_BasicProgModel/Ex_1_1_06.java
@@ -0,0 +1,5 @@
+package U1_1_BasicProgModel;
+
+public class Ex_1_1_06 {
+
+}
