47f351f9b2321207736bc090d44a588f29408c2b
Axel Prieto <prietoaxel@gmail.com>
2017-04-11 20:43:38 -0300

    Daily programming practice

--- _Sedgewick/Sedgewick/src/Main.java
+++ _Sedgewick/Sedgewick/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // Ex_1_1_06.main();
         // Ex_1_1_07.main();
         // Ex_1_1_09.main();
-        Ex_1_1_15.main();
+        // Ex_1_1_15.main();
+        // Ex_1_1_16.main();
     }
 }
