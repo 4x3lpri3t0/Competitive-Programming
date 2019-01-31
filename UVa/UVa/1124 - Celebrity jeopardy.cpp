a0c5754f2fafe5a95a261f757d9869b59681b7a9
Axel Prieto <prietoaxel@gmail.com>
2019-01-31 19:12:17 +0100

    minor changes

---
+++ UVa/UVa/1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,18 @@
+#pragma region Includes
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <iostream>
+using namespace std;
+#pragma endregion
+
+int main() {
+	string s;
+	
+	while (getline(cin, s)) {
+		cout << s << endl;
+	}
+	
+	return 0;
+}
