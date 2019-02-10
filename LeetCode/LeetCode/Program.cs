747d6fb5183ea747bc0f866be0c7e1c960e7beab
Axel Prieto <prietoaxel@gmail.com>
2019-02-10 01:33:56 +0100

    minor changes

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -12,6 +12,7 @@ class Program
         //SortedSquares // Merge sort
         //InsertIntoBST // Trees
         //KClosest // Sorting
+        //CountBattleships // Matrix
 
         // TODO: Improve: https://leetcode.com/submissions/detail/206295901/
         //Console.WriteLine(Solution.ShortestCompletingWord("1s3 PSt", new string[] { "step", "steps", "stripe", "stepple" })); 
