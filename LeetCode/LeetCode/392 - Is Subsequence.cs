527f5c02c6462033fab617808e299c4784b9e8ad
Axel Prieto <prietoaxel@gmail.com>
2019-02-16 12:09:32 +0100

    Daily programming practice

---
+++ LeetCode/LeetCode/392 - Is Subsequence.cs
@@ -0,0 +1,21 @@
+﻿// https://leetcode.com/problems/is-subsequence/
+public partial class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+            return true;
+
+        int sIndex = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (s[sIndex] == t[i])
+                sIndex++;
+
+            if (sIndex == s.Length)
+                return true;
+        }
+
+        return false;
+    }
+}
