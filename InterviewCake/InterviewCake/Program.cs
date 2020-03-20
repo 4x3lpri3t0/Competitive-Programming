88f5bed4c75420a51254311a6289ddf3d59dffac
Axel Prieto <prietoaxel@gmail.com>
2020-03-20 08:28:32 +0100

    minor changes

--- InterviewCake/InterviewCake/Program.cs
+++ InterviewCake/InterviewCake/Program.cs
@@ -1,5 +1,5 @@
 ﻿using InterviewCake.Questions;
-using System.Linq;
+using static InterviewCake.Questions._01___MergingMeetingTimes;
 
 namespace InterviewCake
 {
@@ -9,7 +9,17 @@ namespace InterviewCake
         {
             //_01___stock_price.Main();
             //_02___product_of_other_numbers.Main();
-            _03___highest_product_of_3.Main(); // TODO: Review
+            //_03___highest_product_of_3.Main(); // TODO: Review
+
+            // === 2020 ===
+
+            var meeting1 = new Meeting(2, 3);  // meeting from 10:00 – 10:30 am
+            var meeting2 = new Meeting(6, 9);  // meeting from 12:00 – 1:30 pm
+            var meeting3 = new Meeting(9, 11);
+            var meeting4 = new Meeting(10, 12);
+
+            var meetings = new Meeting[] { meeting1, meeting2, meeting3, meeting4 };
+            _01___MergingMeetingTimes.MergeRanges(meetings);
         }
     }
 }
---
+++ InterviewCake/InterviewCake/Questions/01 - MergingMeetingTimes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewCake.Questions
+{
+    public class _01___MergingMeetingTimes
+    {
+        public class Meeting
+        {
+            public int StartTime { get; set; }
+
+            public int EndTime { get; set; }
+
+            public Meeting(int startTime, int endTime)
+            {
+                // Number of 30 min blocks past 9:00 am
+                StartTime = startTime;
+                EndTime = endTime;
+            }
+
+            public override string ToString()
+            {
+                return $"({StartTime}, {EndTime})";
+            }
+        }
+
+        public static void MergeRanges(Meeting[] meetings)
+        {
+            var mergedMeetings = new List<Meeting>();
+
+            // Order meetings by Start Time
+            meetings = meetings
+                .OrderBy(x => x.StartTime)
+                .ToArray();
+
+            var lastMeeting = meetings[0];
+            for (int i = 1; i < meetings.Length; i++)
+            {
+                var currentMeeting = meetings[i];
+
+                if (lastMeeting.EndTime >= currentMeeting.StartTime)
+                {
+                    // Merge
+                    lastMeeting.EndTime = currentMeeting.EndTime;
+                }
+                else
+                {
+                    mergedMeetings.Add(lastMeeting);
+                    lastMeeting = currentMeeting;
+                }                
+            }
+
+            // Add last one
+            mergedMeetings.Add(lastMeeting);
+
+            // Output
+            foreach (var mergedMeeting in mergedMeetings)
+            {
+                Console.WriteLine(mergedMeeting);
+            }
+        }
+    }
+}
