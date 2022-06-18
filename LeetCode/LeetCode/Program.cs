9f01fed0ed15dcf330ad2e9043955a8e78d30c1f
Axel Prieto <prietoaxel@gmail.com>
2022-06-18 21:55:05 +0100

    Merge repos

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,112 +1,108 @@
-﻿using System;
-
-class Program
+﻿internal class Program
 {
-	static void Main(string[] args)
-	{
-		var solution = new Solution();
+    private static void Main(string[] args)
+    {
+        var solution = new Solution();
 
-		// *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-		// *** *** *** *** Interesting/Hard problems to review *** *** *** *** ***
-		// *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
+        // *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
+        // *** *** *** *** Interesting/Hard problems to review *** *** *** *** ***
+        // *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
 
-		//SortedSquares // Merge sort
-		//InsertIntoBST // Trees
-		//KClosest // Sorting
-		//CountBattleships // Matrix
-		//LetterCasePermutation // Recursion
-		//CountBits // Bitwise
-		//CheckPossibility // Tricky logic
-		//StrWithout3a3b // Greedy
-		//ReconstructQueue // Greedy
-		//FindMinArrowShots // Greedy
-		//MinSwapsCouples // UnionFind                  *** DEBUG ***
-		//FindMaxLength
-		//DiameterOfBinaryTree
-		//LastStoneWeight // Array
-		//ContainsNearbyDuplicate // Nice solution (sliding window + set)
-		//CountBits // Bitwise
-		//CheckValidString
-		//GetHappyString // TODO
-		//Search // Search in Rotated Sorted Array
-		//BstFromPreorder // TODO
+        //SortedSquares // Merge sort
+        //InsertIntoBST // Trees
+        //KClosest // Sorting
+        //CountBattleships // Matrix
+        //LetterCasePermutation // Recursion
+        //CountBits // Bitwise
+        //CheckPossibility // Tricky logic
+        //StrWithout3a3b // Greedy
+        //ReconstructQueue // Greedy
+        //FindMinArrowShots // Greedy
+        //MinSwapsCouples // UnionFind                  *** DEBUG ***
+        //FindMaxLength
+        //DiameterOfBinaryTree
+        //LastStoneWeight // Array
+        //ContainsNearbyDuplicate // Nice solution (sliding window + set)
+        //CountBits // Bitwise
+        //CheckValidString
+        //GetHappyString // TODO
+        //Search // Search in Rotated Sorted Array
+        //BstFromPreorder // TODO
 
-		// TODO: Improve: https://leetcode.com/submissions/detail/206295901/
-		//Console.WriteLine(Solution.ShortestCompletingWord("1s3 PSt", new string[] { "step", "steps", "stripe", "stepple" })); 
+        // TODO: Improve: https://leetcode.com/submissions/detail/206295901/
+        //Console.WriteLine(Solution.ShortestCompletingWord("1s3 PSt", new string[] { "step", "steps", "stripe", "stepple" }));
 
+        // *** 2019 *** DP focus ***
+        //StoneGame // TODO: Refactor to bottom-up
+        //MinFallingPathSum
+        //MincostTickets // TODO: Bottom-up?
+        //Rob
+        //NumArray
 
-		// *** 2019 *** DP focus ***
-		//StoneGame // TODO: Refactor to bottom-up
-		//MinFallingPathSum
-		//MincostTickets // TODO: Bottom-up?
-		//Rob
-		//NumArray
+        // *** 2020 ***
+        //int[] testArray1 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        //solution.MaxSubArray(testArray1);
 
-		// *** 2020 ***
-		//int[] testArray1 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-		//solution.MaxSubArray(testArray1);
+        //int[] testArray2 = { 0, 1, 0, 3, 12 };
+        //solution.MoveZeroes(testArray2);
 
-		//int[] testArray2 = { 0, 1, 0, 3, 12 };
-		//solution.MoveZeroes(testArray2);
+        //Console.WriteLine(solution.CountLargestGroup(13));
+        //Console.WriteLine(solution.CountLargestGroup(2));
+        //Console.WriteLine(solution.CountLargestGroup(15));
+        //Console.WriteLine(solution.CountLargestGroup(24));
 
-		//Console.WriteLine(solution.CountLargestGroup(13));
-		//Console.WriteLine(solution.CountLargestGroup(2));
-		//Console.WriteLine(solution.CountLargestGroup(15));
-		//Console.WriteLine(solution.CountLargestGroup(24));
+        //Console.WriteLine(solution.CanConstruct("annabelle", 2));
+        //Console.WriteLine(solution.CanConstruct("leetcode", 3));
+        //Console.WriteLine(solution.CanConstruct("true", 4));
+        //Console.WriteLine(solution.CanConstruct("yzyzyzyzyzyzyzy", 2));
+        //Console.WriteLine(solution.CanConstruct("cr", 7)); // Should be FALSE
+        //Console.WriteLine(solution.CanConstruct("qlkzenwmmnpkopu", 15)); // Should be TRUE
 
-		//Console.WriteLine(solution.CanConstruct("annabelle", 2));
-		//Console.WriteLine(solution.CanConstruct("leetcode", 3));
-		//Console.WriteLine(solution.CanConstruct("true", 4));
-		//Console.WriteLine(solution.CanConstruct("yzyzyzyzyzyzyzy", 2));
-		//Console.WriteLine(solution.CanConstruct("cr", 7)); // Should be FALSE
-		//Console.WriteLine(solution.CanConstruct("qlkzenwmmnpkopu", 15)); // Should be TRUE
+        //var test = new int[][] {
+        //	new int[] { 1, 2 },
+        //	new int[] { 1, 2 },
+        //	new int[] { 3, 3 },
+        //	new int[] { 1, 5 },
+        //	new int[] { 1, 5 },
+        //};
+        //Console.WriteLine(solution.MaxEvents(test));
 
-		//var test = new int[][] {
-		//	new int[] { 1, 2 },
-		//	new int[] { 1, 2 },
-		//	new int[] { 3, 3 },
-		//	new int[] { 1, 5 },
-		//	new int[] { 1, 5 },
-		//};
-		//Console.WriteLine(solution.MaxEvents(test));
+        //var test = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
 
-		//var test = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        //Console.WriteLine(solution.GroupAnagrams(test));
 
-		//Console.WriteLine(solution.GroupAnagrams(test));
+        //var test = new int[] { 1, 0, 0, 1 };
+        //Console.WriteLine(solution.FindMaxLength(test));
 
-		//var test = new int[] { 1, 0, 0, 1 };
-		//Console.WriteLine(solution.FindMaxLength(test));
+        //var grid = new char[][]
+        //{
+        //	"11110".ToCharArray(),
+        //	"11010".ToCharArray(),
+        //	"11000".ToCharArray(),
+        //	"00000".ToCharArray(),
+        //};
+        //Console.WriteLine(solution.NumIslands(grid));
 
-		//var grid = new char[][]
-		//{
-		//	"11110".ToCharArray(),
-		//	"11010".ToCharArray(),
-		//	"11000".ToCharArray(),
-		//	"00000".ToCharArray(),
-		//};
-		//Console.WriteLine(solution.NumIslands(grid));
+        //var grid = new int[][]
+        //{
+        //	new int[] { 1,3,1 },
+        //	new int[] { 1,5,1 },
+        //	new int[] { 4,2,1 },
+        //};
+        //Console.WriteLine(solution.MinPathSum(grid));
 
-		//var grid = new int[][]
-		//{
-		//	new int[] { 1,3,1 },
-		//	new int[] { 1,5,1 },
-		//	new int[] { 4,2,1 },
-		//};
-		//Console.WriteLine(solution.MinPathSum(grid));
+        //Console.WriteLine(solution.MinStartValue(new int[] { 1, 2 }));
+        //Console.WriteLine(solution.MinStartValue(new int[] { 1, -2, -3 }));
+        //Console.WriteLine(solution.MinStartValue(new int[] { -3, 2, -3, 4, 2 }));
 
-		//Console.WriteLine(solution.MinStartValue(new int[] { 1, 2 })); 
-		//Console.WriteLine(solution.MinStartValue(new int[] { 1, -2, -3 }));
-		//Console.WriteLine(solution.MinStartValue(new int[] { -3, 2, -3, 4, 2 }));
+        //Console.WriteLine(solution.FindMinFibonacciNumbers(7));
+        //Console.WriteLine(solution.FindMinFibonacciNumbers(10));
+        //Console.WriteLine(solution.FindMinFibonacciNumbers(19));
 
-		//Console.WriteLine(solution.FindMinFibonacciNumbers(7));
-		//Console.WriteLine(solution.FindMinFibonacciNumbers(10));
-		//Console.WriteLine(solution.FindMinFibonacciNumbers(19));
-
-		//Console.WriteLine(solution.GetHappyString(1, 3));
-		//Console.WriteLine(solution.GetHappyString(1, 4));
-		//Console.WriteLine(solution.GetHappyString(3, 9));
-		//Console.WriteLine(solution.GetHappyString(2, 7));
-		//Console.WriteLine(solution.GetHappyString(10, 100));
-
-	}
+        //Console.WriteLine(solution.GetHappyString(1, 3));
+        //Console.WriteLine(solution.GetHappyString(1, 4));
+        //Console.WriteLine(solution.GetHappyString(3, 9));
+        //Console.WriteLine(solution.GetHappyString(2, 7));
+        //Console.WriteLine(solution.GetHappyString(10, 100));
+    }
 }
---
+++ TopCoder/TopCoder/SRMs/144 - Div 2 - Easy/Time.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+public class Time
+{
+    public string whatTime(int seconds)
+    {
+        string res;
+
+        int secondsInAnHour = 60 * 60;
+        int secondsInAMinute = 60;
+
+        int hours = 0;
+
+        while (seconds >= secondsInAnHour)
+        {
+            seconds -= secondsInAnHour;
+            hours++;
+        }
+
+        int minutes = 0;
+
+        while (seconds >= secondsInAMinute)
+        {
+            seconds -= secondsInAMinute;
+            minutes++;
+        }
+
+        res = string.Format("{0}:{1}:{2}", hours, minutes, seconds);
+
+        return res;
+    }
+
+    public static void NotMain(String[] args) { }
+
+}
